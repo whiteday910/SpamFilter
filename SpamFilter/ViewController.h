170e28ff504057dc85c517e2da290eda0e470002
hwi <cto@elimsoft.co.kr>
2017-09-20 00:17:07 +0900

    Initial Commit

---
+++ SpamFilter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpamFilter
+//
+//  Created by hwi on 2017. 9. 20..
+//  Copyright © 2017년 hwi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SpamFilter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SpamFilter
+//
+//  Created by hwi on 2017. 9. 20..
+//  Copyright © 2017년 hwi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
